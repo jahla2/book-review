2cea9b0af86dd4bb3f3537902c5e18a34d48ef9f
rocso <rocso@DESKTOP-FTA3M8O>
2024-03-08 05:29:42 +0800

    Define one to many relationship

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        //Book can many have many revies
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function book()
+    {
+        //Each review belongs to one book
+        return $this->belongsTo(Book::class);
+    }
 }
